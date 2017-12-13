06dc8e1bf6e1ada12e7fee758ae30be0d49a8427
wwt3399 <wwt3399@gmail.com>
2017-12-13 18:27:21 +0800

    finish all for 01 to 05

--- exercise/04/fill/Fill.asm
+++ exercise/04/fill/Fill.asm
@@ -12,3 +12,63 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(FOREVER)
+// arr = SCREEN
+	@SCREEN
+	D=A
+	@arr
+	M=D
+
+// n=8192
+	@8192
+	D=A
+	@n
+	M=D
+
+	@i
+	M=0
+(LOOP)
+  // if (i==n) goto ENDLOOP
+	@i
+	D=M
+	@n
+	D=D-M
+	@ENDLOOP
+	D; JEQ
+	
+  // if (*KBD != 0)
+	@KBD
+	D=M     // D = *KBD
+	@ELSE
+	D; JEQ  // if (*KDB==0) goto ELSE
+	
+	//   RAM[arr+i] = -1
+	@arr
+	D=M
+	@i
+	A=D+M
+	M=-1
+	
+	@ENDIF
+	0; JMP
+(ELSE)	
+  // else 
+  //   RAM[arr+i] = 0
+	@arr
+	D=M
+	@i
+	A=D+M
+	M=0
+	
+(ENDIF)
+	
+	// i++
+	@i
+	M=M+1
+	
+	@LOOP
+	0; JMP
+
+(ENDLOOP)
+	@FOREVER
+	0; JMP
--- exercise/04/mult/mult.asm
+++ exercise/04/mult/mult.asm
@@ -7,3 +7,32 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+//R2=0
+    @R2
+    M=0
+//a=R0
+    @R0
+    D=M
+    @a
+    M=D
+(LOOP)
+//  if(a<=0)go to EXIT
+    @a
+    D=M
+    @EXIT
+    D;JLE
+
+//a=a-1
+    @a
+    M=M-1
+//R2=R2+R1
+    @R1
+    D=M
+    @R2
+    M=D+M
+//goto LOOP
+    @LOOP
+    0;JMP
+(EXIT)
+    @EXIT
+    0;JMP
